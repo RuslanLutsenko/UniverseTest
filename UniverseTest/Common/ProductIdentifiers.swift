b9473cd76a3cfebd2bc07645b0501664050744da
RuslanLutsenko <ruslanlu2001@gmail.com>
2023-05-27 10:12:05 +0300

    Feature[UT-3] Added mock subscription product
    Major features:
    -Added mock subscription product
    -Changed project config for "StoreKit configuration"

---
+++ UniverseTest/Common/ProductIdentifiers.swift
@@ -0,0 +1,12 @@
+//
+//  ProductIdentifiers.swift
+//  UniverseAppsTest
+//
+//  Created by Руслан Луценко on 27.05.2023.
+//
+
+import Foundation
+
+enum ProductIdentifiers: String {
+    case monthly = "com.UniverseAppsTest.monthly"
+}
